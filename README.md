10ce25ee014ab1a9b289720bf8e89e31de72c489
vfavorito <vince.favorito@gmail.com>
2020-10-16 21:00:46 -0500

    Updated Javascript and Readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# password-generator
+# Password Generator Javascript
+
+## Random Password Generator
+
+### Overview:
+In this repository I created a random password generator.  The generator is designed to take information given by the user and with this information give a password that meets the user's requirements.  The Criteria used in the password are password length, and whether you wanna use lowercase letters, uppercase letters, numbers, and/or special characters. This was primarily accomplished using Javascript prompts, for loops, functions, and if/else statements.  
+
+## How it works!
+
+### The process goes Like this:
+### Click Generate Password Button
+![PasswordGenBTN](Images/passgenbtn.png)
+### Answer Prompts
+<img src="Images/prompt.PNG" alt="prompt" width="1000"/>
+
+### View Generated Password
+![GeneratedPassword](Images/genpassword.png)
+
+### Example
+Here is a link to the Generator:  [https://vfavorito.github.io/password-generator/](https://vfavorito.github.io/password-generator/)
+
+### Installation
+To install this project simply clone this repository to your local directory and access it using Visual Studio Code
+
+### License
+Copyright <2020> < Vincent Favorito >
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
--- script.js
+++ script.js
@@ -1,8 +1,133 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
+let allChars = {
+  lowerLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  upperLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  specialChars: [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+}
+
+let passString = "";
+
+function generatePassword(length, isLower, isUpper, isNumber, isSpecial) {
+
+  // this is combining all the requested characters used in the password into one array
+  let totalContent = [];
+
+  if (isLower) {
+    for (let i = 0; i < allChars.lowerLetters.length; i++) {
+      totalContent.push(allChars.lowerLetters[i]);
+    }
+  }
+  if (isUpper) {
+    for (let i = 0; i < allChars.upperLetters.length; i++) {
+      totalContent.push(allChars.upperLetters[i]);
+    }
+  }
+  if (isNumber) {
+    for (let i = 0; i < allChars.numbers.length; i++) {
+      totalContent.push(allChars.numbers[i]);
+    }
+  }
+  if (isSpecial) {
+    for (let i = 0; i < allChars.specialChars.length; i++) {
+      totalContent.push(allChars.specialChars[i]);
+    }
+  }
+
+  let passCriteria = [isLower, isUpper, isNumber, isSpecial];
+  let criteriaCheck = passCriteria.includes(true);
+
+  if (criteriaCheck === true && length > 7 && length < 121) {
+
+    let finalPass = [];
+
+    for (let i = 0; i < length; i++) {
+      let randomChar = totalContent[Math.floor(Math.random() * totalContent.length)];
+      finalPass.push(randomChar);
+    }
+    let hasChars = characterTest(finalPass.join(""), isLower, isUpper, isNumber, isSpecial);
+    if(hasChars===true){
+      console.log(finalPass+" finalPass");
+      console.log(finalPass.join("")+ " finalPass join")
+      passString=finalPass.join("");
+      return;
+    }
+    else{
+    generatePassword(length,isLower,isUpper,isNumber,isSpecial);
+    }
+  }
+
+  else {
+    alert("You either did not select any criteria for your password or your password is smaller than 8 or larger than 128 characters long.  Try again");
+    main();
+  }
+}
+
+function characterTest(password, isLower, isUpper, isNumber, isSpecial) {
+  let chars = 0;
+  if (isLower) {
+    chars++;
+  }
+  if (isUpper) {
+    chars++;
+  }
+  if (isNumber) {
+    chars++;
+  }
+  if (isSpecial) {
+    chars++;
+  }
+  let lowerChecked = 0;
+  let upperChecked = 0;
+  let numberChecked = 0;
+  let specialChecked = 0;
+  let checkedChars = 0;
+
+  for (let i = 0; i < password.length; i++) {
+    if (allChars.lowerLetters.join("").includes(password[i]) && lowerChecked === 0) {
+      lowerChecked++;
+      checkedChars++;
+    }
+    else if (allChars.upperLetters.join("").includes(password[i]) && upperChecked === 0) {
+      upperChecked++;
+      checkedChars++;
+    }
+    else if (allChars.numbers.join("").includes(password[i]) && numberChecked === 0) {
+      numberChecked++;
+      checkedChars++;
+    }
+    else if (allChars.specialChars.join("").includes(password[i]) && specialChecked === 0) {
+      specialChecked++;
+      checkedChars++;
+    }
+  }
+
+  console.log(password + " password string")
+  console.log(chars + " chars");
+  console.log(checkedChars +" checked chars");
+  console.log(lowerChecked + " lowerChecked");
+  console.log(upperChecked + " upperChecked");
+  console.log(numberChecked + " numberchecked");
+  console.log(specialChecked + " specialChecked");
+
+  if (chars === checkedChars) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 // The start of the main function triggered by clicking button
-function writePassword() {
+function writePassword(password) {
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
+
+function main() {
   // series of prompts getting info for the password
   let passLengthPrompt = prompt("How many characters? Min: 8 Max: 128");
   // this is taking the string entered in the previous prompt and converting it into a number
@@ -12,63 +137,9 @@ function writePassword() {
   let passUpper = confirm("Do you want uppercase letters in your password? Ok = Yes Cancel = No");
   let passNumeric = confirm("Do you want numeric characters in your password? Ok = Yes Cancel = No");
   let passSpecial = confirm("Do you want special characters in your password? Ok = Yes Cancel = No");
-  // objects of each criteria for password and whether or not they are being used
-  let lowerLetters = {
-    used:passLower,
-    content:["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
-  }
-  let upperLetters = {
-    used:passUpper,
-    content:["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
-  }
-  let numbers = {
-    used:passNumeric,
-    content:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  }
-  let specialChars = {
-    used:passSpecial,
-    content:[" ","!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","[","\\","]","^","_","`","{","|","}","~",]
-  }
-// this is combining all the requested characters used in the password into one array
-  let totalContent =[];
-  if (lowerLetters.used===true){
-    for (let i = 0; i < lowerLetters.content.length; i++) {
-      totalContent.push(lowerLetters.content[i]);
-    }
-  }
-  if (upperLetters.used===true){
-    for (let i = 0; i < upperLetters.content.length; i++) {
-      totalContent.push(upperLetters.content[i]);
-    }
-  }
-  if (numbers.used===true){
-    for (let i = 0; i < numbers.content.length; i++) {
-      totalContent.push(numbers.content[i]);
-    }
-  }
-  if (specialChars.used===true){
-    for (let i = 0; i < specialChars.content.length; i++) {
-      totalContent.push(specialChars.content[i]);
-    }
-  }
-  console.log(totalContent)
-  let passCriteria = [passLower, passUpper, passNumeric, passSpecial];
-  let criteriaCheck = passCriteria.includes(true);
-  function generatePassword() {
-    if (criteriaCheck === true && passLength > 7 && passLength < 121){
-      
-    }
-    else {
-      alert("You either did not select any criteria for your password or your password is smaller than 8 or larger than 120 characters long.  Try again");
-      writePassword();
-    }
-  }
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  generatePassword(passLength, passLower, passUpper, passNumeric, passSpecial);
+  writePassword(passString);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", main);
